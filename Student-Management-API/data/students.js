e2e6d7a1c7cdcdf21fbedad63c135315b2f11eae
prithvisaini1222-a11y <prithvisaini1222@gmail.com>
2026-09-24 22:31:58 +0530

    Add students data to students.js

---
+++ Student-Management-API/data/students.js
@@ -0,0 +1,25 @@
+let students = [
+    {
+        id: 1,
+        name: "Tannu Somani",
+        age: 19,
+        course: "B.Tech CSE",
+        email: "tannu@example.com"
+    },
+    {
+        id: 2,
+        name: "Prithvi",
+        age: 20,
+        course: "B.Tech CSE",
+        email: "prithvi@example.com"
+    },
+    {
+        id: 3,
+        name: "Alok",
+        age: 19,
+        course: "B.Tech AI & ML",
+        email: "alok@example.com"
+    }
+];
+
+module.exports = students;
